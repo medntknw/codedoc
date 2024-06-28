095d28d5382e279d383cab098b9f396e4f1042d1
Aniket Patanwal <aniket.patanwal99@gmail.com>
2024-06-28 21:47:29 +0530

    backend sync

--- codedoc-backend/server.js
+++ codedoc-backend/server.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 const WebSocket = require('ws');
+const Y = require('yjs');
 const { setupWSConnection } = require('y-websocket/bin/utils');
 
 const app = express();
+const allowedOrigins = ['http://localhost', 'http://192.168.29.139', 'https://codedoc-frontend.vercel.app'];
+app.use(cors(
+  {
+    origin: allowedOrigins,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type",
+    credentials: true
+  }
+));
 app.use(express.json());
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const rooms = new Map();
+
 wss.on('connection', (ws, req) => {
-  setupWSConnection(ws, req);
+  const roomId = req.url.split('/')[2];
+  if (!rooms.has(roomId)) {
+    const doc = new Y.Doc();
+    rooms.set(roomId, doc);
+  }
+  const doc = rooms.get(roomId);
+  setupWSConnection(ws, req, {doc});
+});
+
+app.get('/test', (req, res) => {
+  res.send('Hello!');
 });
 
 server.listen(1234, () => {
